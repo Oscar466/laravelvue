b495c9af7a2a3ee7d09c5fa8aa23aedc0a96e623
Oscarfelix <tel.opj@hotmail.cl>
2021-02-12 11:30:43 -0300

    subiendo archivos

---
+++ Formx.vue
@@ -0,0 +1,69 @@
+<template>
+    <div>
+        <h1 class="text-center">Formulario Aws</h1>
+        <hr>
+            <div class="form-group" >
+                <label class="form-label">Param uno</label>
+                <input v-model="datatres.paramUno"  type="number" class="form-control"   >
+            </div>
+            <div class="form-group">
+                <label for="" class="form-label">Param dos</label>
+                <input v-model="datatres.paramDos"  type="number" class="form-control" >
+            </div>
+
+            <div class="form-group">
+                <label  lass="form-label">Arn</label>
+                <input v-model="datatres.arnUno"  type="text" class="form-control" >
+            </div>
+
+        <div class="form-group">
+            <label  lass="form-label">Nombre</label>
+            <input v-model="datatres.nombreUno"  type="text" class="form-control" >
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
+            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+        </div>
+            <div class="mb-3">
+                <button type="submit" class="btn btn-primary" @click="registrar" >Submit</button>
+            </div>
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return{
+               datatres:{
+                   paramUno: "",
+                   paramDos: "",
+                   arnUno: "",
+                   nombreUno: ""
+
+               }
+
+        }
+            },
+
+    methods: {
+        registrar: async function() {
+            await axios.post('/peticionaxios',{
+                datatres: this.datatres,
+
+            }).then(response =>{
+                if(response.status==201){
+                    this.datatres.paramUno == "";
+                }
+            }).catch(e =>{
+                console.log(e);
+            });
+            }
+
+        }
+
+
+}
+</script>
+<style>
+
+</style>
---
+++ Tablas.vue
@@ -0,0 +1,70 @@
+
+<template>
+    <div>
+        <h1 class="text-center">Gestionar Datos AWS</h1>
+        <hr>
+        <table class="table" >
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">id</th>
+                <th scope="col">paramuno</th>
+                <th scope="col">paramdos</th>
+                <th scope="col">arn</th>
+                <th scope="col">resultado</th>
+                <th scope="col">date_of_creation</th>
+                <th scope="col">date_of_last delete</th>
+                <th scope="col">delete_at</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="registros in dataDos" :key="registros.id" >
+                <th scope="row">{{registros.id}}</th>
+                <td>{{registros}}</td>
+                <td>{{registros.paramuno}}</td>
+                <td>{{registros.paramdos}}</td>
+                <td>{{registros.arn}}</td>
+                <td>{{registros.resultado}}</td>
+                <td>{{registros.date_of_creation}}</td>
+                <td>{{registros.date_of_last_delete}}</td>
+                <td>{{registros.delete_at}}</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data() {
+        return {
+            dataDos: [],
+        }
+    },
+    created(){
+        this.list();
+    },
+    beforeMount() {
+        this.list();
+    },
+    methods: {
+        async list() {
+            try{
+                const {
+                    data
+                } = await axios.get('hola');
+                    this.dataDos = data;
+            }catch(ex){
+                console.log(ex);
+            }
+
+
+        }
+
+    }
+
+}
+</script>
+<style>
+
+</style>
